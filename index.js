cf66e96270494b0767509cc01f0a0c17d22f8956
Shane Hunter <shane_hunter@bigpond.com>
2020-09-24 17:37:58 +1000

    mvp commit for sep 24

--- index.js
+++ index.js
@@ -12,28 +12,43 @@ document.getElementById("getCityWeather").addEventListener("submit", (e) => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${queryData}`)
     .then((response) => response.json())
     .then((data) => {
-      const { main, name, weather } = data;
-      const icon = `https://s3-us-west-2.amazonaws.com/s.cpdn.io/162656/${weather[0]["icon"]}.svg`;
-      const li = document.createElement("li");
-      listGroupItem.classList.add("city");
+      console.log(data);
+      const name = data.name;
+      const temp = data.main.temp;
+      const weatherMain = data.weather[0].main;
+      const weatherDesc = data.weather[0].description;
+      const feelsLike = data.main.feels_like;
+      const icon = data.weather[0].icon;
+      const iconIMG = `http://openweathermap.org/img/wn/${icon}.png`;
+
+      //   const { main, name, weather } = data;
+      //   const icon = `https://s3-us-west-2.amazonaws.com/s.cpdn.io/162656/${weather[0]["icon"]}.svg`;
+      //   const li = document.createElement("li");
+      //   listGroupItem.classList.add("city");
       const html = `
-        <div class="list-group-item card card_layout data-name="${name}">
-        <h3 class="city-name" data-name="${name}"><strong>${name}</strong></h3>
-          <div class="city-temp">
-          <h4>Today's Temp: ${main.temp} C</h4>
-          </div>
-          <div class="city-weather">
-          <h4>${weather.main}</h4>
-          <h5>${weather.description}</h5>
-          <h5>Feels like: ${main.feels_like}</h5>
-          </div>
-          <div>
-          <img class="city-icon" src="${icon}" alt="${weather[0]["description"]}">
+        <div class="list-group-item card card_layout w-50 data-name="${name}" style="box-shadow: 5px 5px 7px rgba(202,200,200,0.45"> 
+            <h3 class="city-name" data-name="${name}"><strong>${name}</strong></h3>
+            <hr>
+                <div class="city-temp">
+                    <h4>${Math.round(temp)} &deg;C</h4>
+                </div>
+          
+                <div class="city-weather">
+                    <h4>${weatherMain}</h4>
+                        <img class="city-icon" src="${iconIMG}" width="150" height="150"/>
+                        <h5>${weatherDesc}</h5> 
+                    <hr>
+                    <h5>Feels like: ${Math.round(feelsLike)} &deg;C</h5>
+                </div>
         </div>
-      </div>
-      </div>
         `;
       listGroupItem.innerHTML = html;
-      listGroupItem.appendChild(list - group);
+      listGroupItem.appendChild("list-group");
     });
 });
+
+{
+  /* <div>
+          <img class="city-icon" src="${icon}" alt="${weather[0]["description"]}">
+        </div> */
+}
